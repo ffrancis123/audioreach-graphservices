7def7cc6d0165098256f0946c5728ea76cc00b8c
ffrancis123 <ffrancis@qti.qualcomm.com>
2025-10-29 20:11:14 +0530

    Update build.sh
    Signed-off-by: ffrancis123 <ffrancis@qti.qualcomm.com>

--- ci/build.sh
+++ ci/build.sh
@@ -18,3 +18,5 @@ autoconf --force
 ./configure CFLAGS="-Wno-int-conversion" ${BUILD_ARGS} --with-sanitized-headers=${GITHUB_WORKSPACE}/install/sysroots/armv8-2a-poky-linux/usr/lib/modules/$KERNEL_VERSION/build/include/uapi/sound/qcom
 # make
 make DESTDIR=${GITHUB_WORKSPACE}/build install
+
+# Axiom Test
